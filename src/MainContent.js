298c02e008264a858fea312d8d0248d512e4b159
sanjeewna <sanjeewnadevi93@gmail.com>
2023-11-20 11:12:05 +0200

    week7-lab4 completed

---
+++ src/MainContent.js
@@ -0,0 +1,14 @@
+// MainContent.js
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <p>
+        React is a JavaScript library for building user interfaces. In this lab, we'll create and compose basic React components.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
